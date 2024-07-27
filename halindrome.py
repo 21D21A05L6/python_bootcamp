8eb5bae006e6e98c8d546d35a9cb229a6b3a19db
21D21A05L6 <128461018+21D21A05L6@users.noreply.github.com>
2024-07-27 17:40:20 +0530

    Add files via upload

---
+++ halindrome.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 23 11:01:38 2024
+
+@author: gsrav
+"""
+
+s=['aba','sasdad','aaaccc','tapdog','emepe']
+count=0
+for i in s:
+    if i==i[::-1]:
+        count+=1
+    else:
+        m=len(i)//2
+        if len(i)%2==0:
+            first=i[:m]
+            second=i[m:]
+            if first==first[::-1] or second==second[::-1]:
+                count+=1
+        else:
+            first=i[:m+1]
+            second=i[m+1:]
+            if first==first[::-1] or second==second[::-1]:
+                 count+=1
+print(count)
+             
+        
